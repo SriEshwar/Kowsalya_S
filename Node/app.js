8c859a2c56964135a8b7567dd292d0bdef83fb7d
kowsy2219 <107198638+kowsy2219@users.noreply.github.com>
2024-04-26 14:27:23 +0530

    Add files via upload

--- Node/app.js
+++ Node/app.js
@@ -1,4 +1,4 @@
-const amount=12
+/*const amount=12
 
 if(amount<10){
     console.log("Small number");
@@ -6,3 +6,13 @@ if(amount<10){
 else{
     console.log("Greater number");
 }
+*/
+
+const { log } = require('console');
+const os=require('os');
+
+const user=os.userInfo();
+
+console.log(user);
+
+console.log(`The System uptime is ${os.uptime()} seconds`);
